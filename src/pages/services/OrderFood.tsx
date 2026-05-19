7adaba7d247805e08f1d94f5856a8f89c4977feb
aishnalla1 <nallaaishwarya24@gmail.com>
2026-05-19 21:13:16 +0530

    uploading it

--- src/pages/services/OrderFood.tsx
+++ src/pages/services/OrderFood.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import Layout from "@/components/Layout";
 import VideoHero from "@/components/VideoHero";
@@ -75,7 +74,6 @@ const OrderFood = () => {
             {item.name}
           </p>
         </div>
-
       ))}
     </div>
   </div>
